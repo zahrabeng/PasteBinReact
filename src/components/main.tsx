e1ead9d196440b3dd6a536b08e375378d29aa96a
Zahra <bghzahra@outlook.com>
2022-04-26 14:08:10 +0100

    added function that gets all the data

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -4,12 +4,22 @@ import { useEffect } from "react"
 import axios from "axios";
 
 export default function Main (): JSX.Element {
-    
+    const [paste, setPaste] = useState <PasteTypes>()
+    const herokuURL = "https://pastebinserver.herokuapp.com/pastes"
+
+    useEffect (() => {
+       async function getAllPastes(){
+           const data = await axios.get(herokuURL)
+           setPaste(data.data)
+       }
+       getAllPastes()
+    }, [])
 
 
     
     return (
        <>
+       <div></div>
        </>
     )
 
