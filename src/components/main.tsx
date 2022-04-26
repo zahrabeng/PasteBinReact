67295ca200989c66aca18607be33f65acd472b9e
emanfalola9998 <emmanuelfalola9998@gmail.com>
2022-04-26 15:00:10 +0100

    added select dropdown bar

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 export default function Main (): JSX.Element {
     const [paste, setPaste] = useState <PasteTypes[]>([])
     const [code, setCode] = useState("")
+    const [language, setLanguage] = useState("")
 
     const herokuURL = "https://pastebinserver.herokuapp.com/pastes"
 
@@ -17,14 +18,31 @@ export default function Main (): JSX.Element {
        getAllPastes()
     }, [])
 
+    function dropDownList() :JSX.Element{
+        return(
+            <select>
+                <option onClick={() => setLanguage('Python')} >Python</option>
+                <option onClick={() => setLanguage('Css')} >Css</option>
+                <option onClick={() => setLanguage('Javascript')} >Javascript</option>
+                <option onClick={() => setLanguage('Typescript')} >Typescript</option>
+            </select> 
+        
+
+        )
+
+    }
+    
+
 
     
     const allPastes = paste.map((onePaste:PasteTypes) => (<li key={onePaste.id}>{onePaste.code}{onePaste.language}</li>))
     
     return (
        <>
-    <textarea id="allpastes" name="allpastes" rows={15} cols={80} onChange = {(e) => setCode(e.target.value)}/>        
-    {allPastes}
+        <textarea id="allpastes" name="allpastes" rows={15} cols={80} onChange = {(e) => setCode(e.target.value)}/>     
+        {dropDownList()}
+        {allPastes}
+        {console.log(language)}
        </>
     )
 
