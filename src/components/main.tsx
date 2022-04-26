3320d5a5249c60d9c95a6d2c35c4349141e1d540
Zahra <bghzahra@outlook.com>
2022-04-26 13:53:07 +0100

    installed axios as a module

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,12 @@
-export default function Main (): JSX.Element {
+import PasteTypes from "./pasteTypes"
+import { useState } from "react"
+import { useEffect } from "react"
+import axios from "axios";
 
+export default function Main (): JSX.Element {
     
+
+
     
     return (
        <>
